2c0ca91d7582e7df1a4cc394eb29ba115ea4401c
Audre <audrewow@gmail.com>
2019-10-26 13:23:16 -0400

    Update nav, login, messages

---
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as alertify from 'alertifyjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+  constructor() {}
+
+  confirm(message: string, okCallback: () => any) {
+    alertify.confirm(message, (e: any) => {
+      okCallback();
+    });
+  }
+
+  success(message: any) {
+    alertify.success(message);
+  }
+
+  error(message: any) {
+    alertify.error(message);
+  }
+
+  warning(message: any) {
+    alertify.warning(message);
+  }
+
+  message(message: any) {
+    alertify.message(message);
+  }
+}
