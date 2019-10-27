a48d07c0870cdc585638a01b2dc85bb2d5008ef0
Audre <audrewow@gmail.com>
2019-10-27 14:17:06 -0400

    Add auto mapping for User DTOs

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(destination => destination.PhotoUrl, 
+                options => options.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(destination => destination.Age, options => options.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+            .ForMember(destination => destination.PhotoUrl, 
+                options => options.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(destination => destination.Age, options => options.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailedDto>();
+            
+        }
+    }
+}
